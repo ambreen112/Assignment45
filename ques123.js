9859db6bc6fcb0debde8de50f439ba32eb7e87f5
ambreen112 <160263381+ambreen112@users.noreply.github.com>
2024-03-05 21:20:24 +0500

    Add files via upload

---
+++ ques123.js
@@ -0,0 +1,12 @@
+// Question No # 2
+var _Pnoun = "Hina";
+console.log("Hello", _Pnoun, "would you like to come with me?");
+//Question no # 3
+var PersonName = "Sania Saeed";
+console.log(PersonName.toLowerCase());
+console.log(PersonName.toUpperCase());
+console.log(PersonName.replace(/\b\w/g, function (c) { return c.toUpperCase(); }));
+// Question No # 4
+var Famousperson = ("Hazrat Ali");
+var quote = ("Calmness is a sign of intelligence");
+console.log(Famousperson + " " + quote);
