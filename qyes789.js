5b51ceb3d127ee9ea0270f3416ee95de62de795a
ambreen112 <160263381+ambreen112@users.noreply.github.com>
2024-03-06 10:58:14 +0500

    Add files via upload

---
+++ qyes789.js
@@ -0,0 +1,9 @@
+//Question No # 7
+console.log(4 + 4);
+console.log(12 - 4);
+console.log(4 * 2);
+console.log(16 / 2);
+//Question No # 9
+var favNumber = 16;
+var Mmesge = "My favourite number is:";
+console.log(Mmesge, favNumber);
